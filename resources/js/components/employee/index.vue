8b3421d0b8c6bafa7595fe4d23ff068e3de3e9de
RakibAhmedPranto <rakib820@gmail.com>
2021-04-22 05:30:44 +0600

    branch10

---
+++ resources/js/components/employee/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+        <h1>All Employees</h1>
+	</div>
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,8 +1,16 @@
+// auth Components
 let login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 let forget = require('./components/auth/forgetPassword.vue').default;
 let logout = require('./components/auth/logout.vue').default;
+
+
+//dashboard Components
 let home = require('./components/Home.vue').default;
+
+//Employee Components
+let employeeCreate = require('./components/employee/create.vue').default;
+let employeeIndex = require('./components/employee/index.vue').default;
 
 
 export const routes = [
@@ -11,4 +19,6 @@ export const routes = [
     { path: '/forget', component: forget, name:'forget' },
     { path: '/logout', component: logout, name:'logout' },
     { path: '/home', component: home, name:'home' },
+    { path: '/employee/create', component: employeeCreate, name:'employee.create' },
+    { path: '/employee', component: employeeIndex, name:'employee' },
   ]
