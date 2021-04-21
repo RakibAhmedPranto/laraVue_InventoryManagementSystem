a4b4a45d2cd185105b28a0687b84fca27974a56c
RakibAhmedPranto <rakib820@gmail.com>
2021-04-21 20:16:06 +0600

    sweet alert integrated

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,23 @@ import {routes} from './routes';
 import User from './Helpers/User';
 window.User = User;
 
+//import sweet alert2
+import Swal from 'sweetalert2'
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
+
+window.Toast = Toast;
 
 const router = new VueRouter({
     routes,
